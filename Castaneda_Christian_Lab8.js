24b993cb66772f4e31fefce0c1c91a205183cd73
Chris Castaneda <SLEVEN15@fullsail.edu>
2014-09-20 15:24:31 -0400

    Eight commit: Various Fixes and Flowchart

--- Castaneda_Christian_Lab8.js
+++ Castaneda_Christian_Lab8.js
@@ -7,28 +7,28 @@
 
 // Problem 1
 
-    // var userEmail = prompt("Enter a valid E-Mail address: ");
-// 
-    // var emailFunct = function(email)
-    // {
-        // var userInput = email;
-//         
-        // for(var i = 0; i < userInput.length; i++)
-        // {
-          // if(userInput.indexOf("@") != 0  && userInput.lastIndexOf("@") != (userInput.length - 1) && userInput.indexOf("@") == userInput.lastIndexOf("@"),
-             // userInput.indexOf(".") != 0  && userInput.lastIndexOf(".") != (userInput.length - 1) && userInput.indexOf(".") == userInput.lastIndexOf("."),
-             // userInput.indexOf("@") != -1 && userInput.indexOf(".") != -1 && userInput.indexOf(" ") == -1)
-          // {
-            // console.log("You entered a valid email!");
-            // break;          
-          // }
-          // else
-          // {
-             // console.log("The E-mail address you entered is invald.");
-             // break;
-          // };           
-        // };
-    // };
+    var userEmail = prompt("Enter a valid E-Mail address: ");
+
+    var emailFunct = function(email)
+    {
+        var userInput = email;
+        
+        for(var i = 0; i < userInput.length; i++)
+        {
+          if(userInput.indexOf("@") != 0  && userInput.lastIndexOf("@") != (userInput.length - 1) && userInput.indexOf("@") == userInput.lastIndexOf("@"),
+             userInput.indexOf(".") != 0  && userInput.lastIndexOf(".") != (userInput.length - 1) && userInput.indexOf(".") == userInput.lastIndexOf("."),
+             userInput.indexOf("@") != -1 && userInput.indexOf(".") != -1 && userInput.indexOf(" ") == -1)
+          {
+            console.log("You entered a valid email!");
+            break;          
+          }
+          else
+          {
+             console.log("The E-mail address you entered is invald.");
+             break;
+          };           
+        };
+    };
 
 // Problem 2
     
@@ -48,7 +48,7 @@
 // MAIN
 
     console.log("~~~~~~~~~~~~~~~~~~~~~~PROBLEM ONE~~~~~~~~~~~~~~~~~~~~~~");
-    //emailFunct(userEmail);
+    emailFunct(userEmail);
     
     console.log("~~~~~~~~~~~~~~~~~~~~~~PROBLEM TWO~~~~~~~~~~~~~~~~~~~~~~");
     separators(nums,",","/");
